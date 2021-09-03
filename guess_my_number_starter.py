f6226c49d46e0c6bcd1bb283d6ab03da69b715df
Mylar <iayiku@stu.ucc.edu.gh>
2021-09-03 21:52:40 +0000

    Update guess_my_number_starter.py

--- guess_my_number_starter.py
+++ guess_my_number_starter.py
@@ -10,20 +10,20 @@ import random
 
 # ********************************** YOUR CODE GOES BELOW HERE *********************************************************
 #random number generation
-random_number= random.randint(1,99)
-#defining computers number
-cnum=random_number
+import random
+computer_number= random.randint(1,99)
 guess=None 
-name=input("Please enter your name ").upper()
-print(f" Hello {name} !\n  Welcome To Mylar's Guessing Game")
-while cnum != guess:
+print("     Guess my number\n     It's between [1 -99]")
+while guess!= computer_number:
     guess=int(input('Enter Your Number: '))
-    if guess < random_number:
+    if guess < computer_number:
         print("Sorry your guess is too low ...\n Try again ")
         continue
-    elif guess >random_number:
+    elif guess >computer_number:
         print("sorry your guess is too high ...\n Try again ")
         continue
-    elif guess==random_number:
+    elif guess==computer_number:
         print("Hurray !!! you guessed the number.")
+        print("The number was ",computer_number)
+       
         
